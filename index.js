1fc520a54338fdd691882ede0e513ca09c8c93ba
huskyhoochu <dfg1499@gmail.com>
2018-10-28 20:14:34 +0900

    [#1] x축, y축 그리기
    - 데이터셋을 읽어들이는 Promise 객체 정의
    - Promise 객체로부터 이어지는 비동기 체인 구성
    - d3.js를 이용해 데이터셋을 읽고 x축, y축 그리기

--- index.js
+++ index.js
@@ -1,1 +1,73 @@
-document.write('hi');
+import * as d3 from 'd3';
+import populationCSV from './population.csv';
+
+const populationData = [];
+
+const readCSV = new Promise((resolve) => {
+  resolve(
+    d3.csv(populationCSV, (data) => {
+      const item = {
+        year: parseInt(data.year, 10),
+        population: parseInt(data.population, 10),
+      };
+      populationData.push(item);
+    }),
+  );
+});
+
+readCSV
+  .then(() => {
+    const xMin = d3.min(populationData, d => d.year);
+    const xMax = d3.max(populationData, d => d.year);
+    const yMax = d3.max(populationData, d => d.population);
+
+    return {
+      xMin,
+      xMax,
+      yMax,
+    };
+  })
+  .then((value) => {
+    const xScale = d3
+      .scaleLinear()
+      .domain([value.xMin, value.xMax])
+      .range([0, 740]);
+
+    const yScale = d3
+      .scaleLinear()
+      .domain([10000000, value.yMax])
+      .range([540, 0]);
+
+    return {
+      xScale,
+      yScale,
+    };
+  })
+  .then((value) => {
+    const xAxis = d3.axisBottom(value.xScale).tickFormat(d3.format('d'));
+    const yAxis = d3.axisLeft(value.yScale).ticks(10);
+
+    return {
+      xAxis,
+      yAxis,
+    };
+  })
+  .then((value) => {
+    const svgGraphic = d3
+      .select('#chart')
+      .append('svg')
+      .attr('width', 800)
+      .attr('height', 600);
+
+    svgGraphic
+      .append('g')
+      .attr('class', 'x-axis')
+      .attr('transform', 'translate(60, 540)')
+      .call(value.xAxis);
+
+    svgGraphic
+      .append('g')
+      .attr('class', 'y-axis')
+      .attr('transform', 'translate(60, 0)')
+      .call(value.yAxis);
+  });
