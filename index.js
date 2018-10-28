2186346eadcd96e00c72c6a7bf8931c49ff6efe4
huskyhoochu <dfg1499@gmail.com>
2018-10-29 00:16:33 +0900

    [#1] 차트 그리기 완성
    - 비동기 체인을 없애고 하나의 콜백 함수 안에서 모든 처리를 끝내기로 함
    - (변수들이 서로 필요한 경우가 많아서)
    - line 객체를 만들고 데이터셋을 집어넣어 라인을 그림
    - 라인의 두께와 색상은 css 처리

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import * as d3 from 'd3';
 import populationCSV from './population.csv';
 
+const size = {
+  width: 800,
+  height: 600,
+  margin: 80,
+};
+
 const populationData = [];
 
 const readCSV = new Promise((resolve) => {
@@ -15,59 +21,55 @@ const readCSV = new Promise((resolve) => {
   );
 });
 
-readCSV
-  .then(() => {
-    const xMin = d3.min(populationData, d => d.year);
-    const xMax = d3.max(populationData, d => d.year);
-    const yMax = d3.max(populationData, d => d.population);
+readCSV.then(() => {
+  const xMin = d3.min(populationData, d => d.year);
+  const xMax = d3.max(populationData, d => d.year);
+  const yMax = d3.max(populationData, d => d.population);
 
-    return {
-      xMin,
-      xMax,
-      yMax,
-    };
-  })
-  .then((value) => {
-    const xScale = d3
-      .scaleLinear()
-      .domain([value.xMin, value.xMax])
-      .range([0, 740]);
+  const xScale = d3
+    .scaleLinear()
+    .domain([xMin, xMax])
+    .range([0, size.width - size.margin]);
 
-    const yScale = d3
-      .scaleLinear()
-      .domain([10000000, value.yMax])
-      .range([540, 0]);
+  const yScale = d3
+    .scaleLinear()
+    .domain([10000000, yMax])
+    .range([size.height - size.margin, size.margin]);
 
-    return {
-      xScale,
-      yScale,
-    };
-  })
-  .then((value) => {
-    const xAxis = d3.axisBottom(value.xScale).tickFormat(d3.format('d'));
-    const yAxis = d3.axisLeft(value.yScale).ticks(10);
+  const xAxis = d3.axisBottom(xScale).tickFormat(d3.format('d'));
+  const yAxis = d3.axisLeft(yScale).ticks(10);
 
-    return {
-      xAxis,
-      yAxis,
-    };
-  })
-  .then((value) => {
-    const svgGraphic = d3
-      .select('#chart')
-      .append('svg')
-      .attr('width', 800)
-      .attr('height', 600);
+  const line = d3
+    .line()
+    .x(d => xScale(d.year))
+    .y(d => yScale(d.population))
+    .curve(d3.curveMonotoneX);
 
-    svgGraphic
-      .append('g')
-      .attr('class', 'x-axis')
-      .attr('transform', 'translate(60, 540)')
-      .call(value.xAxis);
+  const svgGraphic = d3
+    .select('#chart')
+    .append('svg')
+    .attr('width', size.width)
+    .attr('height', size.height);
 
-    svgGraphic
-      .append('g')
-      .attr('class', 'y-axis')
-      .attr('transform', 'translate(60, 0)')
-      .call(value.yAxis);
-  });
+  svgGraphic
+    .append('g')
+    .attr('class', 'x-axis')
+    .attr(
+      'transform',
+      `translate(${size.margin}, ${size.height - size.margin})`,
+    )
+    .call(xAxis);
+
+  svgGraphic
+    .append('g')
+    .attr('class', 'y-axis')
+    .attr('transform', `translate(${size.margin}, 0)`)
+    .call(yAxis);
+
+  svgGraphic
+    .append('path')
+    .datum(populationData)
+    .attr('class', 'line')
+    .attr('transform', `translate(${size.margin}, 0)`)
+    .attr('d', line);
+});
