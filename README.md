821e66aa18fef493a41ccbf7fab321756320fc09
huskyhoochu <dfg1499@gmail.com>
2018-10-29 02:24:43 +0900

    [#2] README.md 작성 및 주석 추가
    - 스크린샷 추가

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # Chart Control App
 
+## 개요
+
+주어진 데이터로 웹브라우저 위에 차트를 그리고 그래프의 굵기를 조절할 수 있는 어플리케이션입니다. Node 환경에서 데이터 시각화 패키지인 d3.js를 이용해 차트를 구현했습니다. 번들러는 Webpack보다 가벼운 Parcel을 사용했습니다.
+
+## d3.js
+
+d3.js는 데이터를 시각화하는 모든 방식이 집대성된 패키지입니다. 이것도 pandas와 마찬가지로 csv, xml 파일을 읽어들여 데이터셋을 구성할 수 있었습니다. 다만 x축, y축에 그래프까지 따로 그려야 해서 이런 단순한 차트에 사용하기엔 다소 번거로워 보이기도 했습니다.
+
+
+## 기술 스펙
+
+- d3.js
+- Parcel
+
+## 파일 구조
+
+```
+.
+├ index.js
+├ index.html
+└ index.css
+```
+
+## 설치 및 실행
+
+소스코드 디렉터리로 들어가서 npm 의존성 패키지를 설치합니다.
+
+```
+$ cd chart
+
+# npm
+$ npm install
+
+# yarn
+$ yarn
+```
+
+그리고 Node 서버를 실행합니다.
+
+```
+# npm
+$ npm run start
+
+# yarn
+$ yarn start
+```
+
+## 사용법
+
+![screenshot](./screenshot/main.png)
+
+사용법은 단순합니다. '+' 버튼을 누르면 그래프의 두께가 굵어지고 '-' 버튼을 누르면 가늘어집니다. 더 이상 가늘어질 수 없게 되면 경고 메시지가 뜹니다.
+
+![alert](./screenshot/alert.png)
+
+('최소 굵기입니다' 경고)
+
+![thick](./screenshot/thick.png)
+
+(두꺼워졌을 때의 모습)
--- index.js
+++ index.js
@@ -1,56 +1,71 @@
 import * as d3 from 'd3';
 import populationCSV from './population.csv';
 
+// d3.js로 구현할 차트 박스의 너비, 높이, 마진 값
 const size = {
   width: 800,
   height: 600,
   margin: 80,
 };
 
+// csv 파일에서 읽어들인 데이터를 담는 배열
 const populationData = [];
 
+// 파일을 읽어들인 뒤에 모든 연산이 시작되어야 해서
+// Promise 객체를 이용한 비동기 체인 구현
 const readCSV = new Promise((resolve) => {
   resolve(
+    // csv 파일을 row 단위로 순차적으로 읽어들일 때
     d3.csv(populationCSV, (data) => {
+      // 각 row 에서 year, population 값을 object에 저장
       const item = {
         year: parseInt(data.year, 10),
         population: parseInt(data.population, 10),
       };
+      // 데이터를 담은 object를 배열에 추가
       populationData.push(item);
     }),
   );
 });
 
+// readCSV Promise가 정상적으로 마무리되면 실행
 readCSV.then(() => {
+  // x축(년도)의 최솟값, 최댓값, 그리고 y축(인구)의 최댓값을 구함
   const xMin = d3.min(populationData, d => d.year);
   const xMax = d3.max(populationData, d => d.year);
   const yMax = d3.max(populationData, d => d.population);
 
+  // x축, y축의 스케일
   const xScale = d3
-    .scaleLinear()
-    .domain([xMin, xMax])
-    .range([0, size.width - size.margin]);
+    .scaleLinear() // 선형 스케일
+    .domain([xMin, xMax]) // 축의 최솟값과 최댓값
+    .range([0, size.width - size.margin]); // 축의 길이
 
   const yScale = d3
     .scaleLinear()
     .domain([10000000, yMax])
     .range([size.height - size.margin, size.margin]);
 
+  // x축, y축
+  // tick: 눈금 옵션
   const xAxis = d3.axisBottom(xScale).tickFormat(d3.format('d'));
   const yAxis = d3.axisLeft(yScale).ticks(10);
 
+  // 그래프 라인
   const line = d3
     .line()
-    .x(d => xScale(d.year))
-    .y(d => yScale(d.population))
-    .curve(d3.curveMonotoneX);
+    .x(d => xScale(d.year)) // x 좌표값은 year 사용
+    .y(d => yScale(d.population)) // y 좌표값은 population 사용
+    .curve(d3.curveMonotoneX); // 라인을 부드럽게 처리
 
+  // 캔버스 역할을 하는 svg 삽입하고 너비, 높이 지정
   const svgGraphic = d3
     .select('#chart')
     .append('svg')
     .attr('width', size.width)
     .attr('height', size.height);
 
+  // DOM 위에 x축 그리기
   svgGraphic
     .append('g')
     .attr('class', 'x-axis')
@@ -60,12 +75,14 @@ readCSV.then(() => {
     )
     .call(xAxis);
 
+  // DOM 위에 y축 그리기
   svgGraphic
     .append('g')
     .attr('class', 'y-axis')
     .attr('transform', `translate(${size.margin}, 0)`)
     .call(yAxis);
 
+  // DOM 위에 line 그리기
   svgGraphic
     .append('path')
     .datum(populationData)
@@ -74,22 +91,29 @@ readCSV.then(() => {
     .attr('d', line);
 });
 
+// 굵기 값 변수 지정
 let thicknessValue = 1;
+// 증가 버튼, 감소 버튼 DOM
 const increaseBtn = document.getElementById('increase');
 const decreaseBtn = document.getElementById('decrease');
 
+// 증가 버튼 이벤트 리스너
 increaseBtn.addEventListener('click', () => {
   const line = document.getElementsByClassName('line')[0];
+  // 굵기 값을 1 증가시킨 뒤 line 객체에 적용
   thicknessValue += 1;
   line.style.strokeWidth = thicknessValue;
 });
 
+// 감소 버튼 이벤트 리스너
 decreaseBtn.addEventListener('click', () => {
   const line = document.getElementsByClassName('line')[0];
   if (thicknessValue === 1) {
+    // 조건문: 굵기 값이 1이면 더 이상 굵기를 줄일 수 없다는 경고
     thicknessValue = 1;
     alert('최소 굵기입니다.');
   } else {
+    // 굵기 값이 1보다 크면 1 갑소시킨 뒤 line 객체에 적용
     thicknessValue -= 1;
   }
   line.style.strokeWidth = thicknessValue;
